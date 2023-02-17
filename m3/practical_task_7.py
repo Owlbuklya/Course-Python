825dee96916c7fb89f925650f4aaf766b0d67aa4
NPO-PRIBOR\m.malcev <Owl_buklya@mail.ru>
2023-02-17 15:36:57 +0300

    no message

--- m3/practical_task_7.py
+++ m3/practical_task_7.py
@@ -1,60 +1,69 @@
 # Задача - чат-бот для записи покупок
+import json
 
-shopping_list = {'Хлеб':1} 
+shopping_list = {}
 
 # Функция добавления наименования и кол-ва (с проверкой на число) в список
 def add_position (shopping_list):
-   name = input ('Введите наименование: ')
-   while True:
-      quantity = input('Введите количество: ')
-      quantity = quantity.replace(',','.')
-      try:
-      	quantity = float(quantity)
-      	if quantity % 1 == 0:
-      		quantity = int(quantity)
-      except:
-         print('Пожалуйста, введите число: ')
-         continue
-      else:
-         break
-   if name in shopping_list:
-      shopping_list[name] += quantity     
-   else:
-      shopping_list[name] = quantity
-   return shopping_list
+	with open('shopping_list.json', 'r') as f:
+		shopping_list = json.load(f)
+	name = input ('Введите наименование: ')
+	while True:
+		quantity = input('Введите количество: ')
+		quantity = quantity.replace(',','.')
+		try:
+			quantity = float(quantity)
+			if quantity % 1 == 0:
+				quantity = int(quantity)
+		except:
+		 print('Пожалуйста, введите число: ')
+		 continue
+		else:
+		 break
+	if name in shopping_list:
+		shopping_list[name] += quantity     
+	else:
+		shopping_list[name] = quantity
+	with open('shopping_list.json', 'w') as f:
+		json.dump(shopping_list, f)
+	return shopping_list
 
 # Функция удаление позиции из списка
 def remove_position(shopping_list):
-   name = input('Введите наименование позициии, которую хотите удалить: ')
-   shopping_list.pop(name, None)
-   return shopping_list
+	name = input('Введите наименование позициии, которую хотите удалить: ')
+	shopping_list.pop(name, None)
+	with open('shopping_list.json', 'w') as f:
+		json.dump(shopping_list, f)
+	return shopping_list
 
 # Функция отображения списка
 def show_shopping_list(shopping_list):
-   sort_shopping_list = sorted(shopping_list.keys())
-   i = 0
-   for name in sort_shopping_list:
-      i += 1
-      print(f'''{i}) {name} - {shopping_list[name]}''')
+	with open('shopping_list.json', 'r') as f:
+		shopping_list = json.load(f)	
+	sort_shopping_list = sorted(shopping_list.keys())
+	i = 0
+	for name in sort_shopping_list:
+		i += 1
+		print(f'''{i}) {name} - {shopping_list[name]}''')
 
 # Функция при вводе неверной команды
 def error_command(command):
-   if command != "+" or command != "-" or  command != "@" or  command != "!":
-      print('Неверная команда!')
+	if command != "+" or command != "-" or  command != "!" or  command != "%":
+		print('Неверная команда!')
 
 # Функция обработки команд
 def command_processing(shopping_list):
 	while True:
-	   command = input ('Введите команды: "+" чтобы добавить / "-"  чтобы удалить / "@" чтобы вывести список / "!" чтобы выйти: ')
-	   if command == '+':
-	      shopping_list = add_position (shopping_list)
-	   elif command == '-':
-	      shopping_list = remove_position(shopping_list)  
-	   elif command == '@':
-	      show_shopping_list(shopping_list)
-	   elif command == '!':
-	      break
-	   else: 
-	      error_command(command)
+		command = input ('Введите команды: "+" чтобы добавить / "-"  чтобы удалить / "!" чтобы вывести список / "%" чтобы выйти: ')
+		if command == '+':
+			shopping_list = add_position (shopping_list)
+		elif command == '-':
+			shopping_list = remove_position(shopping_list)  
+		elif command == '!':
+			show_shopping_list(shopping_list)
+		elif command == '%':
+			break
+		else: 
+			error_command(command)
 
 command_processing(shopping_list)
