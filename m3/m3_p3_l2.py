5a6b856cfbe32591758bb00e1fd3dc78d5d93cfe
Owlbuklya <owl_buklya@mail.ru>
2023-03-08 09:02:19 +0300

    Решение задачи модуль 3 часть 3 уровень 2

---
+++ m3/m3_p3_l2.py
@@ -0,0 +1,27 @@
+def textor(string_in, len_word):
+	string_without_punc_sym = ''
+	for symbol in string_in:
+		if symbol.isalnum() or symbol == ' ' or symbol == '\n':
+			string_without_punc_sym += symbol
+	str_list = string_without_punc_sym.split()
+	list_out = []
+	for word in str_list:
+		if len(word) == len_word:
+			list_out.append(word)
+	return list_out
+
+n = 4 # длина слов для поиска 
+
+s = '''Было просто пасмурно, дуло с севера
+А к обеду насчитал сто градаций серого.
+Так всегда первого ноль девятого
+То ли весь мир сошёл с ума, то ли я - того.
+На столе записка от неё смятая
+Недопитый виски допиваю с матами.
+Посмотрю в окно, допишу главу
+Первое сентября, дворник жжёт листву.
+Серым облакам наплевать на нас
+Если знаешь как жить - живи
+Ты хотела плыть как все - так плыви!'''
+
+print(textor(s, n))
